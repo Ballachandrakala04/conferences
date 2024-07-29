4e00c39cffc01dbdd05c1d846029dcc4d03025f2
chandrakala balla <86072716+Ballachandrakala04@users.noreply.github.com>
2024-07-29 16:22:14 +0530

    Update login.php
    yes h1 tag we chaged

--- login.php
+++ login.php
@@ -8,6 +8,7 @@ include('db_connect.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
+     <title>Login Form2222</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
